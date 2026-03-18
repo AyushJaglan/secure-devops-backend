8d3121ab54e49dd519cc62b2932d9eb2ef205fe8
Ayush Jaglan <ayushjaglan@mac.nuigalway.ie>
2026-03-18 11:45:13 +0000

    XSS ready for ZAP

--- app.js
+++ app.js
@@ -6,18 +6,18 @@ app.use(express.json());
 const userRoutes = require('./routes/user');
 app.use('/api', userRoutes);
 
-app.listen(3000, () => {
-    console.log('Server running on port 3000');
-
-const password = "123456"; // hardcoded secret
-
-app.get('/test', (req, res) => {
-    eval("console.log('unsafe')");
-    res.send("test route");
-    
 app.get('/search', (req, res) => {
   const query = req.query.q;
   res.send(`<h1>${query}</h1>`); // XSS vulnerability
 });
+app.get('/test', (req, res) => {
+    eval("console.log('unsafe')");
+    res.send("test route");
 });
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
+
+const password = "123456"; // hardcoded secret
+    
+
 });
